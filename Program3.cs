326a702205bf745438653efeaa9fe56453a3a325
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-08-30 09:17:15 +0530

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee1
+{
+        public partial class Employee11
+        {
+            public int EmployeeId { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public double BaseSalary { get; set; }
+            public int YearsOfWork { get; set; }
+        static void Main(string[] args)
+        {
+            Employee11 employee = new Employee11
+            {
+                EmployeeId = 1,
+                FirstName = "Payal",
+                LastName = "Bansal",
+                BaseSalary = 50000,
+                YearsOfWork = 7
+            };
+
+            employee.DisplayEmployeeDetails();
+            Console.ReadKey();
+        }
+    }
+    
+}
+
