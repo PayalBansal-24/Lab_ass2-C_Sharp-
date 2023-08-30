52552b21ff9854699c6021ec62cf25313f810172
PayalBansal-24 <137813552+PayalBansal-24@users.noreply.github.com>
2023-08-30 09:16:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee1
+{
+        public partial class Employee11
+        {
+            public double CalculateSalary()
+            {
+                double salary = BaseSalary;
+
+                if (YearsOfWork >= 5)
+                {
+                    salary += 1000;
+                }
+
+                return salary;
+            }
+
+            public void DisplayEmployeeDetails()
+            {
+                Console.WriteLine($"Employee ID: {EmployeeId}");
+                Console.WriteLine($"Name: {FirstName} {LastName}");
+                Console.WriteLine($"Base Salary: ${BaseSalary:F2}");
+                Console.WriteLine($"Years of Work: {YearsOfWork}");
+                Console.WriteLine($"Calculated Salary: ${CalculateSalary():F2}");
+            }
+        }
+ 
+}
